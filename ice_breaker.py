98b6eb4dcf6559f33dd8aaabfbb17e85fe332306
Wilson Moraes dos Santos <wilsontads@gmail.com>
2025-01-06 17:36:45 -0300

    Add initial requirements.txt with dependencies

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,2 +1,8 @@
-if __name__ == '__main__':
+import os
+
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    load_dotenv()
     print("Hello LangChain!")
+    print(f"Hello {os.getenv('COL_API_KEY')}!")
