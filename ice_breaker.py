fcc198c189afac9b4c3f79730f655c0e7b856215
Wilson Moraes dos Santos <wilsontads@gmail.com>
2025-03-14 10:31:50 -0300

    Refactor icebreaker to use LinkedIn agent integration

--- ice_breaker.py
+++ ice_breaker.py
@@ -6,31 +6,42 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agend
+
+
+def ice_break_with_agent(name: str) -> str:
+    linkedin_profile_username = linkedin_lookup_agend(name)
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_profile_username
+    )
+
+    summary_template = """
+        given the Linkedin information {information} about a person I want you to create in portuguese:
+        1. A short summary
+        2. list the negative points of his profile
+        """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model="gpt-4o-mini")
+
+    chain = summary_prompt_template | llm
+
+    parser = StrOutputParser()
+
+    res = parser.parse(chain.invoke(input={"information": linkedin_data}))
+    match = re.search(r"content='(.*?)'", str(res), re.DOTALL)
+    formatted_output = match.group(1)
+    return formatted_output
+
 
 if __name__ == "__main__":
     load_dotenv()
 
     print("Hello LangChain")
 
-    summary_template = """
-    given the Linkedin information {information} about a person I want you to create in portuguese:
-    1. A short summary
-    2. list the negative points of his profile
-    """
+    ice_break_agent_wilson = ice_break_with_agent(name="Wilson Moraes dos Santos")
 
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
-    )
-
-    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
-
-    chain = summary_prompt_template | llm
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/wilsonmoraes/"
-    )
-    parser = StrOutputParser()
-
-    res = parser.parse(chain.invoke(input={"information": linkedin_data}))
-    match = re.search(r"content='(.*?)'", str(res), re.DOTALL)
-    formatted_output = match.group(1)
-    print(formatted_output)
+    print(ice_break_agent_wilson)
